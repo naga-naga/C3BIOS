7781a9d09004b793e676224b67c619e6dba4114b
naga-naga <58594938+naga-naga@users.noreply.github.com>
2020-01-21 17:48:02 +0900

    Add files via upload

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,17 +3,21 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	private Label label_time; // 現在時刻を表示するラベル
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("TopPage.fxml"));
 			Scene scene = new Scene(root,600,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setResizable(false); // ウインドウサイズ変更不可
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
